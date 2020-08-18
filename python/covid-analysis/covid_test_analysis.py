aad79cc0e381a4a75d7ce205d31e22237e31fde8
Timon Florian Godt <timon.godt@live.com>
2020-08-18 17:00:48 +0200

    Added python directory and covid-19 data

---
+++ python/covid-analysis/covid_test_analysis.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pylpot as plt 
+import seaborn as sns
+
+# import dataset 
+df = pd.read_csv(/mleng/covid-19-data/public/data/testing/covid-testing-all-observations.csv)
+
+print(df)
